6961f30772e88a8229818b26d8d706ead81aaa9f
Bebe Peng <bebepeng@gmail.com>
2014-05-05 15:08:44 -0600

    whitespace

--- tracker.rb
+++ tracker.rb
@@ -13,15 +13,15 @@ class Tracker
   end
 
   def stories(project_name)
-    id = projects.select {|project| project['name'] == project_name}.first['id']
+    id = projects.select { |project| project['name'] == project_name }.first['id']
     response = Faraday.get "https://www.pivotaltracker.com/services/v5/projects/#{id}/stories?date_format=millis&with_state=unstarted", {}, 'X-TrackerToken' => @api_token
     tracker_data = JSON.parse(response.body)
-    tracker_data.map {|story|{'id' => story['id'], 'name' => story['name']} }
+    tracker_data.map { |story| {'id' => story['id'], 'name' => story['name']} }
   end
 
   def details(project_name, story_name)
-    project_id = projects.select {|project| project['name'] == project_name}.first['id']
-    story_id = stories(project_name).select {|story| story['name'] == story_name}.first['id']
+    project_id = projects.select { |project| project['name'] == project_name }.first['id']
+    story_id = stories(project_name).select { |story| story['name'] == story_name }.first['id']
     response = Faraday.get "https://www.pivotaltracker.com/services/v5/projects/#{project_id}/stories/#{story_id}", {}, 'X-TrackerToken' => @api_token
     tracker_data = JSON.parse(response.body)['description']
   end
