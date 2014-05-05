fac10b4d234921d4a620a46fec3f1321435964ee
Bebe Peng <bebepeng@gmail.com>
2014-05-05 14:13:24 -0600

    users can get the 7 day weather forecast

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -8,5 +8,7 @@ describe Weather do
   it 'displays the current weather' do
     expect(@weather.forecast).to eq "few clouds"
   end
-  it 'displays the 7 day forcast'
+  it 'displays the 7 day forcast' do
+    expect(@weather.seven_day).to eq ["few clouds", "scattered clouds", "light rain", "light rain", "light rain", "moderate rain", "light rain"]
+  end
 end
--- weather.rb
+++ weather.rb
@@ -3,10 +3,20 @@ require 'json'
 
 class Weather
   def initialize(city)
-    response = Faraday.get "http://api.openweathermap.org/data/2.5/weather?q=#{city}"
-    @weather_data = JSON.parse(response.body)
+    @city = city
   end
+
   def forecast
-   @weather_data["weather"].first["description"]
+    response = Faraday.get "http://api.openweathermap.org/data/2.5/weather?q=#{@city}"
+    weather_data = JSON.parse(response.body)
+    weather_data["weather"].first["description"]
+  end
+
+  def seven_day
+    response = Faraday.get "http://api.openweathermap.org/data/2.5/forecast/daily?q=#{@city}&mode=json&cnt=7"
+    weather_data = JSON.parse(response.body)
+    weather_data["list"].each_with_object([]) do |day, weeks_forecast|
+      weeks_forecast << day["weather"].first["description"]
+    end
   end
 end
